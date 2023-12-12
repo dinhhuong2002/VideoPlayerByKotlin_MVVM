e6ed45c6e915200a9ba2b855c920f23e2070ba6c
dinhhuong2002 <dinhthihuongdung2002@gmail.com>
2023-12-12 18:20:15 +0700

    Add: videoItems.xml in libs, DhPlayerView.kt, DhPlayer.kt

---
+++ Player/src/main/java/com/example/DhPlayerView.kt
@@ -0,0 +1,34 @@
+package com.example
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.core.view.isVisible
+import com.example.player.R
+
+class DhPlayerView(context: Context, attrs: AttributeSet?=null) : FrameLayout(context, attrs) {
+
+
+    //initMethod
+    private fun init(){
+        View.inflate(context, R.layout.dhplayer_view,this)
+    }
+
+    //bind items
+    private fun bindItems(){
+
+        val ic_play=findViewById<ImageView>(R.id.ic_play)
+        val ic_forward=findViewById<ImageView>(R.id.ic_foward)
+        val ic_replay=findViewById<ImageView>(R.id.ic_replay)
+        var tv_total_time=findViewById<TextView>(R.id.total_time)
+        var tv_progress_time=findViewById<TextView>(R.id.progress_time)
+        var seekbar=findViewById<SeekBar>(R.id.seekBar)
+
+    }
+
+
+}
